e72355fa439d83639f503f5681158ae9e6828f88
xbhp1984@163.com <xzb@163.com>
2022-12-31 23:24:08 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pay = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.up = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.info = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectmusic = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PlayerMusic = new AxWMPLib.AxWindowsMediaPlayer();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerMusic)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pay
+            // 
+            this.pay.Location = new System.Drawing.Point(183, 263);
+            this.pay.Name = "pay";
+            this.pay.Size = new System.Drawing.Size(75, 23);
+            this.pay.TabIndex = 1;
+            this.pay.Text = "播放";
+            this.pay.UseVisualStyleBackColor = true;
+            this.pay.Click += new System.EventHandler(this.pay_Click);
+            // 
+            // pause
+            // 
+            this.pause.Location = new System.Drawing.Point(264, 263);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(75, 23);
+            this.pause.TabIndex = 1;
+            this.pause.Text = "停止";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(345, 263);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 1;
+            this.next.Text = "下一曲";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(426, 263);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(75, 23);
+            this.up.TabIndex = 1;
+            this.up.Text = "上一曲";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(28, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(112, 220);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // info
+            // 
+            this.info.AutoSize = true;
+            this.info.Location = new System.Drawing.Point(140, 330);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(0, 12);
+            this.info.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "当前播放的音乐";
+            // 
+            // selectmusic
+            // 
+            this.selectmusic.Location = new System.Drawing.Point(37, 12);
+            this.selectmusic.Name = "selectmusic";
+            this.selectmusic.Size = new System.Drawing.Size(75, 23);
+            this.selectmusic.TabIndex = 5;
+            this.selectmusic.Text = "选择音乐";
+            this.selectmusic.UseVisualStyleBackColor = true;
+            this.selectmusic.Click += new System.EventHandler(this.selectmusic_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PlayerMusic
+            // 
+            this.PlayerMusic.Enabled = true;
+            this.PlayerMusic.Location = new System.Drawing.Point(171, 26);
+            this.PlayerMusic.Name = "PlayerMusic";
+            this.PlayerMusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("PlayerMusic.OcxState")));
+            this.PlayerMusic.Size = new System.Drawing.Size(360, 215);
+            this.PlayerMusic.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.selectmusic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.pay);
+            this.Controls.Add(this.PlayerMusic);
+            this.Name = "Form1";
+            this.Text = "2023音乐播放器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerMusic)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer PlayerMusic;
+        private System.Windows.Forms.Button pay;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label info;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button selectmusic;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsMusic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();//用于储存音乐的全路径
+        bool b = true;
+        private void pay_Click(object sender, EventArgs e)
+        {
+            if (pay.Text == "播放")
+            {
+                if (b)
+                {
+                    //获得选中的歌曲  让音乐从头播放
+                    PlayerMusic.URL = list[listBox1.SelectedIndex];
+                }
+                PlayerMusic.Ctlcontrols.play();
+                pay.Text = "暂停";
+            }
+            else if (pay.Text == "暂停")
+            {
+                PlayerMusic.Ctlcontrols.pause();
+                pay.Text = "播放";
+                b = false;
+            }
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            PlayerMusic.Ctlcontrols.stop();//停止
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            //获得当前选中的索引
+            int a = listBox1.SelectedIndex + 1;
+            //清空所有选中的索引   这里是因为我们开启了多选属性，才需要清理
+            listBox1.SelectedIndices.Clear();
+            if (a == listBox1.Items.Count)
+            {
+                a = 0;
+            }
+            //将改变后的索引重新赋值给当前选中项的索引
+            listBox1.SelectedIndex = a;
+            PlayerMusic.URL = list[a];
+            PlayerMusic.Ctlcontrols.play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //在程序加载的时候，取消播放器的自动播放功能
+            PlayerMusic.settings.autoStart = false;
+            PlayerMusic.URL = @"E:\KuGou\张杰&张碧晨 - 只要平凡.mp3";
+
+           // label1.Image = Image.FromFile(@"C:\Users\14505\Desktop\继续.jpg");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("请选择音乐！");
+                return;
+            }
+            try
+            {
+                PlayerMusic.URL = list[listBox1.SelectedIndex];
+                PlayerMusic.Ctlcontrols.play();
+                pay.Text = "暂停";
+                PlayerMusic.Text = PlayerMusic.Ctlcontrols.currentPosition.ToString();
+            }
+            catch { }
+        }
+
+        private void selectmusic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择您的文件";
+            ofd.Filter = "音乐文件|*.mp3|全部文件|*.*";
+            ofd.InitialDirectory = @"E:\CloudMusic";
+            ofd.Multiselect = true;
+            ofd.ShowDialog();
+
+            //获得在文本框中选择的全路径
+            string[] path = ofd.FileNames;
+            for (int i = 0; i < path.Length; i++)
+            {
+                list.Add(path[i]);
+                //将音乐文件的文件名存储到listbox中
+
+                listBox1.Items.Add(Path.GetFileName(path[i]));
+            }
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            int a = listBox1.SelectedIndex - 1;
+            listBox1.SelectedIndices.Clear();
+            if (a < 0)
+            {
+                a = listBox1.Items.Count - 1;
+            }
+            //将改变后的索引重新赋值给当前选中项的索引
+            listBox1.SelectedIndex = a;
+            PlayerMusic.URL = list[a];
+            PlayerMusic.Ctlcontrols.play();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //如果播放器的状态时正在播放中
+            if (PlayerMusic.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                info.Text = PlayerMusic.currentMedia.duration.ToString() + "\r\n" + PlayerMusic.currentMedia.durationString + "\r\n" + PlayerMusic.Ctlcontrols.currentPositionString;
+
+                double b1 = double.Parse(PlayerMusic.currentMedia.duration.ToString());
+                double b2 = double.Parse(PlayerMusic.Ctlcontrols.currentPosition.ToString()) + 1;
+                //如果歌曲当前的播放时间等于歌曲的总时间，自动播放下一曲    //比较时间的值
+                if (b1 <= b2)
+                {
+                    //获得当前选中的索引
+                    int a = listBox1.SelectedIndex + 1;
+                    //清空所有选中的索引
+                    listBox1.SelectedIndices.Clear();
+                    if (a == listBox1.Items.Count)
+                    {
+                        a = 0;
+                    }
+                    //将改变后的索引重新赋值给当前选中项的索引
+                    listBox1.SelectedIndex = a;
+                    PlayerMusic.URL = list[a];
+                    PlayerMusic.Ctlcontrols.play();
+                }
+            }
+            //比较时间的值
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //先删集合
+            //首先获得要删除的歌曲的数量
+            int count = listBox1.SelectedItems.Count;
+            for (int i = 0; i < count; i++)
+            {
+                //先删集合
+                list.RemoveAt(listBox1.SelectedIndex);
+                //在删列表
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+    }
+}
